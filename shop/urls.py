91146677811996c7fd95f07262169f4ed6a6519f
Antoni D <Antoni.d@DroiDal.ai>
2025-02-17 22:07:09 +0530

    about page changes

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('shop/', views.shop, name='shop'),
     path('collection/', collection, name='collection'),
-    path('reviews/', views.reviews, name='reviews')
+    path('reviews/', views.reviews, name='reviews'),
     path('about/', views.about, name='about'),
 ]
